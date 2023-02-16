a61b440b843f1e52d4aacac0862f583fbb47e2fd
Satinder Singh Saini <swv095@gmail.com>
2023-02-16 12:47:53 +0530

    Created Spring Project with Spring Security

---
+++ src/main/java/com/learnspringsecurity/springsecuritybasics/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.learnspringsecurity.springsecuritybasics.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping(path = "/welcome")
+    public String welcome(){
+        return "Welcome to Spring Security Application without Security";
+    }
+}
